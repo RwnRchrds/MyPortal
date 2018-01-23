fbfe4b8250ae6efb0f2008ca158f46cb9ca8a6d7
r-richards <rear-view9>
2018-01-23 20:30:42 +0000

    Added remaining controllers and index views.

--- MyPortal/Controllers/StudentsController.cs
+++ MyPortal/Controllers/StudentsController.cs
@@ -10,10 +10,16 @@ namespace MyPortal.Controllers
     public class StudentsController : Controller
     {
         // GET: Students
+        public ActionResult Index()
+        {
+            return View();
+        }
         public ActionResult Random()
         {
             var student = new Student() {Id = 1, FirstName = "Aaron", LastName = "Aardvark"};
             return View(student);
         }
+
+  
     }
 }
