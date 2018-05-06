769e99b60432be93772106ce584559ea71ce8278
Rowan-Richards <11richr1@NEVADA>
2018-05-06 10:31:00 +0100

    Added Placeholder for Markbook Link

---
+++ MyPortal/Scripts/MyPortal/StaffNavBar.js
@@ -0,0 +1,7 @@
+﻿//Markbook Placeholder
+$(document).ready(function () {
+    $("#markbook-link").on("click",
+        function() {
+            toastr.warning("This feature is not yet available");
+        });
+});
