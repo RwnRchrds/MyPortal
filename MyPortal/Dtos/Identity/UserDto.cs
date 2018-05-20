a720f241e8dc809386eda6e4324f3bb94688a53f
Rowan-Richards <11richr1@NEVADA>
2018-05-20 13:29:26 +0100

    User Creation Implemented

--- MyPortal/Dtos/Identity/UserDto.cs
+++ MyPortal/Dtos/Identity/UserDto.cs
@@ -8,6 +8,9 @@ namespace MyPortal.Dtos.Identity
     public class UserDto
     {
         public string Id { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public bool PhoneNumberConfirmed { get; set; }
+        public bool TwoFactorEnabled { get; set; }
         public string UserName { get; set; }
         public string PasswordHash { get; set; }
         public string SecurityStamp { get; set; }
--- MyPortal/Models/ChangePasswordModel.cs
+++ MyPortal/Models/ChangePasswordModel.cs
@@ -11,6 +11,7 @@ namespace MyPortal.Models
         [Required]
         public string UserId { get; set; }
         [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
