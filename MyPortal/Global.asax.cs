d9acb8000befb1e9a82203b7aca00a40b000b536
Rowan-Richards <11richr1@NEVADA>
2018-06-07 20:06:37 +0100

    In-House HTTPS Redirect

--- MyPortal/Global.asax.cs
+++ MyPortal/Global.asax.cs
@@ -23,5 +23,18 @@ namespace MyPortal
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        protected void Application_BeginRequest()
+        {
+            if (!Context.Request.IsSecureConnection
+                && !Context.Request.IsLocal // to avoid switching to https when local testing
+            )
+            {
+                Response.Clear();
+                Response.Status = "301 Moved Permanently";
+                Response.AddHeader("Location", Context.Request.Url.ToString().Insert(4, "s"));
+                Response.End();
+            }
+        }
     }
 }
