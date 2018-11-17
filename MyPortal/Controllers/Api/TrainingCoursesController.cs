8b43825b8e9c59af02f8aebce1f88ebb898aee54
Rowan-Richards <11richr1@NEVADA>
2018-11-17 13:59:12 +0000

    Training Course area updated

--- MyPortal/Controllers/Api/TrainingCoursesController.cs
+++ MyPortal/Controllers/Api/TrainingCoursesController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web.Http;
 using AutoMapper;
 using MyPortal.Dtos;
@@ -22,12 +23,60 @@ namespace MyPortal.Controllers.Api
             _context.Dispose();
         }
 
+        [HttpGet]
         [Route("api/courses")]
         public IEnumerable<TrainingCourseDto> GetCourses()
         {
             return _context.TrainingCourses
                 .ToList()
                 .Select(Mapper.Map<TrainingCourse, TrainingCourseDto>);
+        }
+
+        [HttpPost]
+        [Route("api/courses/edit")]
+        public IHttpActionResult UpdateCourse(TrainingCourseDto course)
+        {
+            var courseInDb = _context.TrainingCourses.Single(x => x.Id == course.Id);
+
+            if (courseInDb == null)
+                return Content(HttpStatusCode.NotFound, "Training course not found");
+
+            courseInDb.Code = course.Code;
+            courseInDb.Description = course.Description;
+
+            _context.SaveChanges();
+
+            return Ok("Training course updated");
+        }
+
+        [HttpGet]
+        [Route("api/courses/fetch/{courseId}")]
+        public TrainingCourseDto GetCourse(int courseId)
+        {
+            var courseInDb = _context.TrainingCourses.Single(x => x.Id == courseId);
+
+            if (courseInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return Mapper.Map<TrainingCourse, TrainingCourseDto>(courseInDb);
+        }
+
+        [HttpDelete]
+        [Route("api/courses/remove/{courseId}")]
+        public IHttpActionResult DeleteCourse(int courseId)
+        {
+            var courseInDb = _context.TrainingCourses.Single(x => x.Id == courseId);
+
+            if (courseInDb == null)
+                return Content(HttpStatusCode.NotFound, "Training course not found");
+
+            if (courseInDb.TrainingCertificates.Any())
+                return Content(HttpStatusCode.BadRequest, "Cannot delete course that has issued certificates");
+
+            _context.TrainingCourses.Remove(courseInDb);
+            _context.SaveChanges();
+
+            return Ok("Training course deleted");
         }
     }
 }
---
+++ MyPortal/ViewModels/TrainingCoursesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MyPortal.Models;
+
+namespace MyPortal.ViewModels
+{
+    public class TrainingCoursesViewModel
+    {
+        public TrainingCourse Course { get; set; }
+    }
+}
