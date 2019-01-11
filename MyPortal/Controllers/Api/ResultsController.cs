d11417905222e5c2c2b66d1ada79cffb475b8fb1
Rowan-Richards <11richr1@NEVADA>
2019-01-11 13:04:42 +0000

    Upload Results method completed --  not tested

--- MyPortal/Controllers/Api/ResultsController.cs
+++ MyPortal/Controllers/Api/ResultsController.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Web;
 using System.Web.Http;
 using AutoMapper;
 using MyPortal.Dtos;
@@ -61,5 +64,42 @@ namespace MyPortal.Controllers.Api
         #region Gradebook Results Management
 
         #endregion
+
+        public IHttpActionResult UploadResults(HttpPostedFileBase file, int resultSetId)
+        {
+            if (file == null || Path.GetExtension(file.FileName) != ".csv")
+            {
+                return Content(HttpStatusCode.BadRequest, "Invalid results file");
+            }
+
+            var subjects = _context.Subjects.OrderBy(x => x.Name).ToList();
+
+            using (var reader = new StreamReader(file.FileName))
+            {
+                reader.ReadLine();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    if (line == null) continue;
+                    var values = line.Split(',');
+                    for (int i = 0; i < subjects.Count; i++)
+                    {
+                        var student = _context.Students.SingleOrDefault(x => x.MisId == values[4]);
+                        if (student == null) continue;
+                        var result = new Result
+                        {
+                            StudentId = student.Id,
+                            ResultSetId = resultSetId,
+                            SubjectId = subjects[i].Id,
+                            Value = values[5 + i]
+                        };
+                        _context.Results.Add(result);
+                    }
+                }
+            }
+
+            _context.SaveChanges();
+            return Ok("Results imported");
+        }
     }
 }
