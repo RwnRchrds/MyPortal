52b7118bfb5d40a65ef5ddb79fe0bb3ed1dffe4b
Rowan-Richards <11richr1@NEVADA>
2019-08-03 04:54:06 +0100

    Extended tests

--- MyPortal.UnitTests/ProcessTests/PeopleProcessesTests.cs
+++ MyPortal.UnitTests/ProcessTests/PeopleProcessesTests.cs
@@ -1,3 +1,4 @@
+using System.IO.Compression;
 using System.Linq;
 using AutoMapper;
 using MyPortal.Models.Database;
@@ -19,6 +20,56 @@ namespace MyPortal.UnitTests.ProcessTests
             Assert.That(result.ResponseType == ResponseType.Ok);
             
             Assert.That(result.ResponseObject.Person.LastName == "Cobb");
+        }
+
+        [Test]
+        public static void GetStaffFromUserId_ReturnsNotFound()
+        {
+            var result = PeopleProcesses.GetStaffFromUserId("testme", _context);
+
+            Assert.That(result.ResponseType == ResponseType.NotFound);
+        }
+
+        [Test]
+        public static void GetStudentFromUserId_ReturnsStudent()
+        {
+            var result = PeopleProcesses.GetStudentFromUserId("aardvark", _context);
+
+            Assert.That(result.ResponseType == ResponseType.Ok);
+
+            Assert.That(result.ResponseObject.Person.LastName == "Aardvark");
+        }
+
+        [Test]
+        public static void GetStudentFromUserId_ReturnsNotFound()
+        {
+            var result = PeopleProcesses.GetStudentFromUserId("testme", _context);
+
+            Assert.That(result.ResponseType == ResponseType.NotFound);
+        }
+
+        [Test]
+        public static void GetStudentDisplayName_ReturnsDisplayName()
+        {
+            var student = _context.Students.SingleOrDefault(x => x.Person.LastName == "Aardvark");
+
+            var result = PeopleProcesses.GetStudentDisplayName(student);
+
+            Assert.That(result.ResponseType == ResponseType.Ok);
+
+            Assert.That(result.ResponseObject == "Aardvark, Aaron");
+        }
+
+        [Test]
+        public static void GetStaffDisplayName_ReturnsDisplayName()
+        {
+            var staff = _context.StaffMembers.SingleOrDefault(x => x.Person.LastName == "Sprague");
+
+            var result = PeopleProcesses.GetStaffDisplayName(staff);
+
+            Assert.That(result.ResponseType == ResponseType.Ok);
+
+            Assert.That(result.ResponseObject == "Mrs L Sprague");
         }
     }
 }
