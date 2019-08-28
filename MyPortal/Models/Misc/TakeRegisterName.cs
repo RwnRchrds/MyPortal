548d9b0ad50008de6260c58f96b7304b81d6fb49
Rowan-Richards <11richr1@NEVADA>
2019-08-28 01:18:04 +0100

    Save-able register

---
+++ MyPortal/Models/Misc/TakeRegisterName.cs
@@ -0,0 +1,10 @@
+using MyPortal.Dtos.LiteDtos;
+
+namespace MyPortal.Models.Misc
+{
+    public class TakeRegisterName
+    {
+        public string StudentName { get; set; }
+        public AttendanceMarkLite Mark { get; set; }
+    }
+}
