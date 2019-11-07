ba2c2cb5017fd56632677d84b39d07a8f4b2b4ce
Rowan <Rowan@PERSEUS>
2019-11-07 17:53:06 +0000

    Identity Updated

--- MyPortal/Models/IdentityModels.cs
+++ MyPortal/Models/IdentityModels.cs
@@ -40,8 +40,10 @@ namespace MyPortal.Models
 
         public ApplicationRole(string name) : base(name)
         {
+            System = false;
         }
 
+        public string Description { get; set; }
         public bool System { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
