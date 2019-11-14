9ad76b1824c51e02ae8f6fb75a9722c0c2b85d06
Rowan <Rowan@PERSEUS>
2019-11-14 16:00:52 +0000

    Fixed icons and updated styles

--- MyPortal/Areas/Staff/Controllers/AssessmentController.cs
+++ MyPortal/Areas/Staff/Controllers/AssessmentController.cs
@@ -15,37 +15,6 @@ namespace MyPortal.Areas.Staff.Controllers
     [RoutePrefix("Assessment")]
     public class AssessmentController : MyPortalController
     {
-        [RequiresPermission("ImportResults")]
-        [Route("Results/Import")]
-        public async Task<ActionResult> ImportResults()
-        {
-            using (var assessmentService = new AssessmentService(UnitOfWork))
-            {
-                var resultSets = await assessmentService.GetAllResultSets();
-                var fileExists = System.IO.File.Exists(@"C:/MyPortal/Files/Results/import.csv");
-                var viewModel = new ImportResultsViewModel
-                {
-                    ResultSets = resultSets,
-                    FileExists = fileExists
-                };
-
-                return View(viewModel);
-            }
-        }
-
-        [HttpPost]
-        [RequiresPermission("ImportResults")]
-        public ActionResult UploadResults(HttpPostedFileBase file)
-        {
-            if (file.ContentLength <= 0 || Path.GetExtension(file.FileName) != ".csv")
-                return RedirectToAction("ImportResults");
-            const string path = @"C:/MyPortal/Files/Results/import.csv";
-            file.SaveAs(path);
-
-            return RedirectToAction("ImportResults");
-        }
-
-        
         [RequiresPermission("EditResultSets")]
         [Route("ResultSets")]
         public ActionResult ResultSets()
