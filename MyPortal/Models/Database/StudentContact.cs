b18dc678d86899bad79feb29db48cb14f88bb2cb
Rowan <Rowan@PERSEUS>
2019-11-28 17:22:17 +0000

    Added StudentContacts Table

---
+++ MyPortal/Models/Database/StudentContact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MyPortal.Models.Database
+{
+    [Table("People_StudentContacts")]
+    public class StudentContact
+    {
+        public int Id { get; set; }
+        public int ContactTypeId { get; set; }
+        public int StudentId { get; set; }
+        public int ContactId { get; set; }
+
+        public bool Correspondence { get; set; }
+        public bool ParentalResponsibility { get; set; }
+        public bool PupilReport { get; set; }
+        public bool CourtOrder { get; set; }
+    }
+}
