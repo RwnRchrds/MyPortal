e8e2d8782b365e49b4084165b5100b15aed00dd9
Rowan Richards <11RichR1@harrodsschool.co.uk>
2020-07-22 18:10:23 +0100

    Fixed issue with student repository

--- MyPortal.Database/Interfaces/Repositories/IStudentRepository.cs
+++ MyPortal.Database/Interfaces/Repositories/IStudentRepository.cs
@@ -9,8 +9,6 @@ namespace MyPortal.Database.Interfaces.Repositories
     public interface IStudentRepository : IReadWriteRepository<Student>
     {
         Task<Student> GetByUserId(Guid userId);
-        Task<IEnumerable<Student>> GetByRegGroup(Guid regGroupId);
-        Task<IEnumerable<Student>> GetByYearGroup(Guid yearGroupId);
         Task<Student> GetByPersonId(Guid personId);
         Task<IEnumerable<Student>> GetAll(StudentSearchOptions searchParams);
         Task<IEnumerable<Student>> GetGiftedTalented();
