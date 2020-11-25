21afb3973bfdbd979ae18a747f3da867da305c18
Rowan Richards <rwnrchrds@gmail.com>
2020-11-25 19:12:17 +0000

    Fixed web component

--- MyPortalWeb/Startup.cs
+++ MyPortalWeb/Startup.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MyPortalWeb.Extensions;
-using MyPortalWeb.Swagger;
 
 namespace MyPortalWeb
 {
