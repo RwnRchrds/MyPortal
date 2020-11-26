3c05dc9f8fbeef88935a342ffcd5c5a615eb4cb1
Rowan Richards <rwnrchrds@gmail.com>
2020-11-26 16:36:29 +0000

    Added terms to academic year

---
+++ MyPortal.Database/Models/Entity/AcademicTerm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MyPortal.Database.Models.Entity
+{
+    [Table("AcademicTerm")]
+    public class AcademicTerm : BaseTypes.Entity
+    {
+        [Column(Order = 1)]
+        public Guid AcademicYearId { get; set; }
+
+        [Column(Order = 2)]
+        [StringLength(128)]
+        public string Name { get; set; }
+
+        [Column(Order = 3)]
+        public DateTime StartDate { get; set; }
+
+        [Column(Order = 4)]
+        public DateTime EndDate { get; set; }
+
+        public virtual AcademicYear AcademicYear { get; set; }
+    }
+}
--- MyPortal.Database/Models/Entity/AcademicYear.cs
+++ MyPortal.Database/Models/Entity/AcademicYear.cs
@@ -16,6 +16,7 @@ namespace MyPortal.Database.Models.Entity
             LogNotes= new HashSet<LogNote>();
             Achievements = new HashSet<Achievement>();
             Incidents = new HashSet<Incident>();
+            AcademicTerms = new HashSet<AcademicTerm>();
         }
 
         [Column(Order = 1)]
@@ -23,12 +24,6 @@ namespace MyPortal.Database.Models.Entity
         [StringLength(128)]
         public string Name { get; set; }
 
-        [Column(TypeName = "date", Order = 2)]
-        public DateTime FirstDate { get; set; }
-
-        [Column(TypeName = "date", Order = 3)]
-        public DateTime LastDate { get; set; }
-        
         [Column(Order = 4)] 
         public bool Locked { get; set; }
 
@@ -43,7 +38,10 @@ namespace MyPortal.Database.Models.Entity
         
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Achievement> Achievements { get; set; }
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Incident> Incidents { get; set; }
+
+        public virtual ICollection<AcademicTerm> AcademicTerms { get; set; }
     }
 }
