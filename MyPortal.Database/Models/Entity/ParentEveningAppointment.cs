fdb6111ef07d31448de4c668d5217ce3472b79cf
Rowan Richards <rwnrchrds@gmail.com>
2021-02-16 21:49:22 +0000

    Added parents' evening entities

---
+++ MyPortal.Database/Models/Entity/ParentEveningAppointment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyPortal.Database.Models.Entity
+{
+    [Table("ParentEveningAppointments")]
+    public class ParentEveningAppointment : Database.BaseTypes.Entity
+    {
+        [Column(Order = 1)]
+        public Guid ParentEveningStaffId { get; set; }
+
+        [Column(Order = 2)]
+        public Guid StudentId { get; set; }
+
+        [Column(Order = 3)]
+        public DateTime Start { get; set; }
+
+        [Column(Order = 4)]
+        public DateTime End { get; set; }
+
+        [Column(Order = 5)]
+        public bool? Attended { get; set; }
+
+        public ParentEveningStaffMember ParentEveningStaffMember { get; set; }
+        public Student Student { get; set; }
+    }
+}
---
+++ MyPortal.Database/Models/Entity/ParentEveningBreak.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MyPortal.Database.Models.Entity
+{
+    [Table("ParentEveningBreaks")]
+    public class ParentEveningBreak : BaseTypes.Entity
+    {
+        [Column(Order = 1)]
+        public Guid ParentEveningStaffMemberId { get; set; }
+
+        [Column(Order = 2)]
+        public DateTime Start { get; set; }
+
+        [Column(Order = 3)]
+        public DateTime End { get; set; }
+
+        public virtual ParentEveningStaffMember ParentEveningStaffMember { get; set; }
+    }
+}
