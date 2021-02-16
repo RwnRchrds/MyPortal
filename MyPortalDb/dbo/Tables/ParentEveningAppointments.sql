b5309def5cd268d7cf5f5a3ab1f45ebd59bf4416
Rowan Richards <rwnrchrds@gmail.com>
2021-02-16 22:00:29 +0000

    Updated db model

---
+++ MyPortalDb/dbo/Tables/ParentEveningAppointments.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[ParentEveningAppointments] (
+    [Id]                   UNIQUEIDENTIFIER DEFAULT (newsequentialid()) NOT NULL,
+    [ParentEveningStaffId] UNIQUEIDENTIFIER NOT NULL,
+    [StudentId]            UNIQUEIDENTIFIER NOT NULL,
+    [Start]                DATETIME2 (7)    NOT NULL,
+    [End]                  DATETIME2 (7)    NOT NULL,
+    [Attended]             BIT              NULL,
+    CONSTRAINT [PK_ParentEveningAppointments] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_ParentEveningAppointments_ParentEveningStaffMembers_ParentEveningStaffId] FOREIGN KEY ([ParentEveningStaffId]) REFERENCES [dbo].[ParentEveningStaffMembers] ([Id]),
+    CONSTRAINT [FK_ParentEveningAppointments_Students_StudentId] FOREIGN KEY ([StudentId]) REFERENCES [dbo].[Students] ([Id])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_ParentEveningAppointments_ParentEveningStaffId]
+    ON [dbo].[ParentEveningAppointments]([ParentEveningStaffId] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_ParentEveningAppointments_StudentId]
+    ON [dbo].[ParentEveningAppointments]([StudentId] ASC);
+
---
+++ MyPortalDb/dbo/Tables/ParentEvenings.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[ParentEvenings] (
+    [Id]            UNIQUEIDENTIFIER DEFAULT (newsequentialid()) NOT NULL,
+    [EventId]       UNIQUEIDENTIFIER NOT NULL,
+    [BookingOpened] DATETIME2 (7)    NOT NULL,
+    [BookingClosed] DATETIME2 (7)    NOT NULL,
+    CONSTRAINT [PK_ParentEvenings] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_ParentEvenings_DiaryEvents_EventId] FOREIGN KEY ([EventId]) REFERENCES [dbo].[DiaryEvents] ([Id])
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IX_ParentEvenings_EventId]
+    ON [dbo].[ParentEvenings]([EventId] ASC);
+
