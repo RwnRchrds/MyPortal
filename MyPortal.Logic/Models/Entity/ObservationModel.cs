cd34df498c2530dc278963043ee0aae9b58caec0
Rowan Richards <rwnrchrds@gmail.com>
2021-10-15 11:41:05 +0100

    wip models

--- MyPortal.Logic/Models/Entity/ObservationModel.cs
+++ MyPortal.Logic/Models/Entity/ObservationModel.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MyPortal.Database.Interfaces;
+using MyPortal.Database.Models.Entity;
+using MyPortal.Logic.Interfaces;
 using MyPortal.Logic.Models.Data;
+using Task = System.Threading.Tasks.Task;
 
 namespace MyPortal.Logic.Models.Entity
 {
-    public class ObservationModel : BaseModel
+    public class ObservationModel : BaseModel, ILoadable
     {
+        public ObservationModel(Observation model) : base(model)
+        {
+            LoadFromModel(model);
+        }
+
+        private void LoadFromModel(Observation model)
+        {
+            Date = model.Date;
+            ObserveeId = model.ObserveeId;
+            ObserverId = model.ObserverId;
+            OutcomeId = model.OutcomeId;
+
+            if (model.Observee != null)
+            {
+                Observee = new StaffMemberModel(model.Observee);
+            }
+
+            if (model.Observer != null)
+            {
+                Observer = new StaffMemberModel(model.Observer);
+            }
+
+            if (model.Outcome != null)
+            {
+                Outcome = new ObservationOutcomeModel(model.Outcome);
+            }
+        }
+
         public DateTime Date { get; set; }
         public Guid ObserveeId { get; set; }
         public Guid ObserverId { get; set; }
@@ -15,5 +47,12 @@ namespace MyPortal.Logic.Models.Entity
         public virtual StaffMemberModel Observee { get; set; }
         public virtual StaffMemberModel Observer { get; set; }
         public virtual ObservationOutcomeModel Outcome { get; set; }
+        
+        public async Task Load(IUnitOfWork unitOfWork)
+        {
+            var model = await unitOfWork.Observations.GetById(Id);
+            
+            LoadFromModel(model);
+        }
     }
 }
--- MyPortal.Logic/Models/Entity/ObservationOutcomeModel.cs
+++ MyPortal.Logic/Models/Entity/ObservationOutcomeModel.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
+using MyPortal.Database.Models.Entity;
 using MyPortal.Logic.Models.Data;
 
 namespace MyPortal.Logic.Models.Entity
 {
     public class ObservationOutcomeModel : LookupItemModel
     {
+        public ObservationOutcomeModel(ObservationOutcome model) : base(model)
+        {
+            ColourCode = model.ColourCode;
+        }
+        
         [StringLength(128)]
         public string ColourCode { get; set; }
     }
