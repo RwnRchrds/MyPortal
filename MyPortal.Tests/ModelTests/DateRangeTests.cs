3fb23617acb7a24dc39933c925d3b7ab48b15d18
Rowan Richards <rwnrchrds@gmail.com>
2022-09-07 12:04:09 +0100

    Added DateRange tests

---
+++ MyPortal.Tests/ModelTests/DateRangeTests.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using MyPortal.Logic.Models.Data;
+using NUnit.Framework;
+using Org.BouncyCastle.Asn1.X509;
+
+namespace MyPortal.Tests.ModelTests;
+
+[TestFixture]
+public class DateRangeTests
+{
+    [Test]
+    public void Overlaps_DetectsStartBeforeEnd()
+    {
+        // A:       |---------------|
+        // B: |---------------|
+
+        var a = new DateRange(DateTime.Today.AddHours(9), DateTime.Today.AddHours(13));
+        var b = new DateRange(DateTime.Today.AddHours(10.5), DateTime.Today.AddHours(15));
+
+        bool result = a.Overlaps(b);
+        
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void Overlaps_DetectsEndAfterStart()
+    {
+        // A: |---------------|
+        // B:         |---------------|
+
+        var a = new DateRange(DateTime.Today.AddHours(10.5), DateTime.Today.AddHours(15));
+        var b = new DateRange(DateTime.Today.AddHours(9), DateTime.Today.AddHours(13));
+
+        bool result = a.Overlaps(b);
+        
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void Overlaps_DetectsStartsBeforeEndAfter()
+    {
+        // A: |------------------------------|
+        // B:           |-------|            
+
+        var a = new DateRange(DateTime.Today.AddHours(9), DateTime.Today.AddHours(18));
+        var b = new DateRange(DateTime.Today.AddHours(13), DateTime.Today.AddHours(14));
+
+        bool result = a.Overlaps(b);
+        
+        Assert.That(result, Is.True);
+    }
+    
+    [Test]
+    public void Overlaps_DetectsDuring()
+    {
+        // A:           |-------|
+        // B: |------------------------------|
+
+        var a = new DateRange(DateTime.Today.AddHours(13), DateTime.Today.AddHours(14));
+        var b = new DateRange(DateTime.Today.AddHours(9), DateTime.Today.AddHours(18));
+
+        bool result = a.Overlaps(b);
+        
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void Overlaps_DetectsExact()
+    {
+        // A: |---------------|
+        // B: |---------------|
+        
+        var a = new DateRange(DateTime.Today.AddHours(9), DateTime.Today.AddHours(12));
+        var b = new DateRange(DateTime.Today.AddHours(9), DateTime.Today.AddHours(12));
+
+        bool result = a.Overlaps(b);
+        
+        Assert.That(result, Is.True);
+    }
+    
+    [Test]
+    [Ignore("This creates extra complication and might not be needed")]
+    public void Overlaps_IgnoresAdjacent()
+    {
+        // A: |---------------|
+        // B:                 |---------------|
+
+        var a = new DateRange(DateTime.Today.AddHours(9), DateTime.Today.AddHours(10));
+        var b = new DateRange(DateTime.Today.AddHours(10), DateTime.Today.AddHours(11));
+
+        bool result = a.Overlaps(b);
+        
+        Assert.That(result, Is.False);
+    }
+    
+    [Test]
+    public void Overlaps_IgnoresSeparate()
+    {
+        // A: |---------------|
+        // B:                       |---------------|
+
+        var a = new DateRange(DateTime.Today.AddHours(9), DateTime.Today.AddHours(10));
+        var b = new DateRange(DateTime.Today.AddHours(12), DateTime.Today.AddHours(13));
+
+        bool result = a.Overlaps(b);
+        
+        Assert.That(result, Is.False);
+    }
+}
